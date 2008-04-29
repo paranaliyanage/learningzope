69f243816ccdd377be3b7c4d8646f307bf9c6887
paranaliyanage <paranaliyanage@2b811298-f24b-0410-8f1f-db691d0b3fe0>
2008-04-29 13:29:16 +0000

    classes moved to a separate folder for better managability.

--- trunk/store.py
+++
@@ -1,9 +0,0 @@
-#store.py - Store a simple string in the database
-
-from testzodb import TestZODB, transaction
-db = TestZODB('./StoreData.fs')
-dbroot = db.dbroot
-dbroot['store_string'] = 'Charith is doing coding for learning ZODB'
-dbroot['store_message'] = 'First example with ZODB!!'
-transaction.commit()
-db.close()
