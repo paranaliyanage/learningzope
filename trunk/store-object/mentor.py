7d0bf83581986aaa310466478322fff7528e6e0e
paranaliyanage <paranaliyanage@2b811298-f24b-0410-8f1f-db691d0b3fe0>
2008-05-14 17:52:59 +0000

    change folder names since the sign "-" brought errors when importing modules.

--- trunk/store-object/mentor.py
+++
@@ -1,8 +0,0 @@
-#mentor.py - Class represnts a GSoC mentor
-
-from persistent import Persistent
-
-class Mentor(Persistent):
-    def __init__(self,id,name):
-        self.id = id
-        self.name = name
