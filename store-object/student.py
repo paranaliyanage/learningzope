c15b4b7c66df8306d8e855a4ef846e025ef539b2
paranaliyanage <paranaliyanage@2b811298-f24b-0410-8f1f-db691d0b3fe0>
2008-05-14 17:52:59 +0000

    change folder names since the sign "-" brought errors when importing modules.

--- store-object/student.py
+++
@@ -1,8 +0,0 @@
-#student.py - Class represents a GSoC student
-
-from persistent import Persistent
-
-class Student(Persistent):
-    def __init__(self,id,name):
-        self.id = id
-        self.name = name
