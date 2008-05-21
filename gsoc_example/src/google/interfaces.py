97262c8d5607531285d67f6da6e7de2ab9cf56ca
paranaliyanage <paranaliyanage@2b811298-f24b-0410-8f1f-db691d0b3fe0>
2008-05-21 10:59:39 +0000

    Change the badly inserted configurations

--- gsoc_example/src/google/interfaces.py
+++ gsoc_example/src/google/interfaces.py
@@ -6,6 +6,29 @@ from zope.app.container.interfaces import IContainer, IContained
 from zope.app.file.interfaces import IFile
 _ = MessageFactory("google")
 
+class IOrganization(Interface):
+    """An Organization object. This contains projects, mentors and students"""
+
+    name=TextLine(
+        title=u"Organization Name",
+        description=u"Name of the organization",
+        default=u"",
+        required=True)
+
+class IGsoc(IContainer):
+    """This is the root object of this project. It can only caontain IOrganization objects"""
+
+    def __setitem__(name, object):
+        """Add a IOrganization object"""
+
+    __setitem__.precondition = ItemTypePrecondition(IOrganization)
+
+    description = Text(
+        title=u"Description",
+        description=u"A detailed description about the content on the Gsoc",
+        default=u"",
+        required=False)
+
 class IProject(Interface):
     """A Project object."""
 
@@ -15,7 +38,7 @@ class IProject(Interface):
 #        description=u"Name of the project",
 #        default=u"",
 #        required=True)
-    
+
 #    description=TextLine(
 #        title=u"Project Description",
 #        description=u"Detailed description about the project",
@@ -47,50 +70,18 @@ class IMentor(Interface):
 #        default=u"",
 #        required=True)
 
-
-class IOrganization(IContainer):
-    """An Organization object. This contains projects, mentors and students"""
-    
-    def __setitem__(name, object):
-        """Add students mentors and projects"""
-
-    __setitem__.precondition = ItemTypePrecondition(IProject, IStudent, IMentor)
-
-    name=TextLine(
-        title=u"Organization Name",
-        description=u"Name of the organization",
-        default=u"",
-        required=True)
-
-
-class IGsoc(IContainer):
-    """This is the root object of this project. It can only caontain IOrganization objects"""
-
-    def __setitem__(name, object):
-        """Add a IOrganization object"""
-
-    __setitem__.precondition = ItemTypePrecondition(IOrganization)
-
-    description = Text(
-        title=u"Description",
-        description=u"A detailed description about the content on the Gsoc",
-        default=u"",
-        required=False)
-
 class IOrganizationContained(IContained):
     """Interface that specifies type of objects that can contain in an organization"""
-    
+
     __parent__ = Field(
         constraint = ContainerTypesConstraint(IGsoc))
 
 
 class IOrganizationContainer(IContainer):
     """Organization is also a container for projects, students and mentors"""
-    
+
     def __setitem__(name,object):
         """Add an IStudent, IProject and IMentor object"""
 
     __setitem__.precondition = ItemTypePrecondition(IProject,IStudent, IMentor)
-
-
 
